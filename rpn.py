1dc8f107d7efbad47c47c3fb1b673dfccb7e8ab9
zeilmatt <zeilmatt@umich.edu>
2018-11-07 01:43:26 -0500

    Initial commit

---
+++ rpn.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import operator
+
+op = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,
+        '^': operator.pow
+        }
+def calculate(arg):
+    # stack for calculator
+    # tokenize input
+    tokens = arg.split()
+    stack = []
+    # do tokens
+    for token in tokens:
+        try:
+            value = int(token)
+            stack.append(value)
+        except ValueError:
+            val2 = stack.pop()
+            val1 = stack.pop()
+
+            # Look up function in table
+            func = op[token]
+            result = func(val1, val2)
+            stack.append(result)
+    return stack[-1]
+
+def main():
+    while True:
+        print(calculate(input('rpn calc> ')))
+
+if __name__ == '__main__':
+    main()
---
+++ test_rpn.py
@@ -0,0 +1,27 @@
+import unittest
+import rpn
+
+class TestBasics(unittest.TestCase):
+    def test_add(self):
+        result = rpn.calculate("1 1 +")
+        self.assertEqual(2, result)
+    
+    def test_sub(self):
+        result = rpn.calculate("4 3 -")
+        self.assertEqual(1, result)
+
+    def test_mult(self):
+        result = rpn.calculate("5 6 *")
+        self.assertEqual(30, result)
+
+    def test_div(self):
+        result = rpn.calculate("69 3 /")
+        self.assertEqual(23, result)
+
+    def test_chain(self):
+        result = rpn.calculate("1 1 + 2 *")
+        self.assertEqual(4, result)
+
+    def test_exp(self):
+        result = rpn.calculate("3 4 ^")
+        self.assertEqual(81, result)
